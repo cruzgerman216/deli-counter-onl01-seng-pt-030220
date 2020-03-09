56bdb17b44f40da53cb4cb35732ccec291badb26
German Cruz <cruzgerman216@yahoo.com>
2020-03-09 05:59:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,5 +19,9 @@ puts "Welcome, #{name}. You are number #{deli.length} in line."
 end
 
 def now_serving(deli)
-
+if deli.empty?
+  puts "There is nobody waiting to be served!"
+else
+  puts "Currently serving #{deli.first}."
+   deli.shift
 end
