5452d2ff2b4864817b4f9ce7a4ec9e4671a33a48
German Cruz <cruzgerman216@yahoo.com>
2020-03-09 05:53:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(deli)
   if deli.empty?
     puts "The line is currently empty."
   else
-    str = "The line is currently:" 
+    str = "The line is currently:"
     deli.each.with_index(1) do|person, i|
       current_line << " #{i}. #{{person}}"
     end
